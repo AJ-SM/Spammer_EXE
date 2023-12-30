3ab903fc9a21b323f5fd22d024764d12c7dbe668
AJ-SM <154649323+AJ-SM@users.noreply.github.com>
2023-12-30 19:42:14 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+
+import pyautogui
+import time
+import random
+i = 0
+ph = ["Too Kise Hai App Log", "Room Room Bhaiyoooo","Good Night", "Good Morning","Good AfterNoon","Jai Hind","HI","Hello","Bye","Adious","Tata","See You"]
+while True:
+    #num = random.randint(0,len(ph)-1)
+    pyautogui.write("Nahi")
+    pyautogui.press("enter")
+    i = i + 1
+    time.sleep(0.1)
